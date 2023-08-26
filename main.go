6a839ef9a301e247734b259931ac2af4ca938606
Diego Cristóbal Herreros <diecristher@gmail.com>
2023-08-26 14:28:47 +0200

    Feat: add all members and groups

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 
-	"github.com/pulumi/pulumi-github/sdk/v4/go/github"
+	"github.com/pulumi/pulumi-github/sdk/v5/go/github"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"gopkg.in/yaml.v2"
 )
